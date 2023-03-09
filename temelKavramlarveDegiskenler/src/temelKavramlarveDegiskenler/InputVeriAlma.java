9fd2bcc1159218cedad3aaa234ed9c2e281d7b09
gizemkisa <gizemkisa@gizems-mbp.home>
2023-03-09 17:15:06 +0300

    kullanıcıdan veri alma hatırlatma çalışması eklendi

---
+++ temelKavramlarveDegiskenler/src/temelKavramlarveDegiskenler/InputVeriAlma.java
@@ -0,0 +1,51 @@
+package temelKavramlarveDegiskenler;
+
+import java.util.Scanner;
+
+public class InputVeriAlma {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+	/*	int a = input. nextInt();
+		double b = input.nextDouble();
+		String c = input.nextLine();
+		float d = input.nextFloat();
+		byte d = input.next.Byte();
+		short e = input.nextShort();
+		*/
+		
+		/*int a,b;
+		
+		System.out.println("A sayısını giriniz : ");
+		
+		a=input.nextInt();
+		
+		System.out.println("B sayısını giriniz : ");
+		
+		b=input.nextInt();
+		
+		System.out.println("A sayısı : " + a);
+		
+		System.out.println("B sayısı : " + b);
+		
+		System.out.println("Bu sayıların toplamı : " + (a+b));
+		*/
+		
+		String str;
+		System.out.print("Adınızı giriniz :" );
+		str = input.nextLine();
+		System.out.println("adınız :" + str);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
